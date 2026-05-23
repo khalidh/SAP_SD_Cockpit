d6f538a235516f95f23f806375b7ab65506437d9
khalidh <3045275+khalidh@users.noreply.github.com>
2026-05-23 23:07:12 +0200

    Serve UI5 resources from CAP

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const path = require("path");
+const fs = require("fs");
+const os = require("os");
 const express = require("express");
 const cds = require("@sap/cds");
 
 module.exports = async function (options) {
   const app = express();
   const root = __dirname;
+  const frameworkRoot = path.join(os.homedir(), ".ui5", "framework", "packages");
 
   app.use((req, res, next) => {
     const origin = req.headers.origin;
@@ -24,6 +27,22 @@ module.exports = async function (options) {
     res.sendFile(path.join(root, "index.html"));
   });
   app.use("/webapp", express.static(path.join(root, "webapp")));
+
+  [
+    "@openui5/sap.ui.core",
+    "@openui5/sap.m",
+    "@openui5/sap.f",
+    "@openui5/sap.ui.layout",
+    "@openui5/sap.ui.table",
+    "@openui5/sap.ui.unified",
+    "@openui5/sap.uxap",
+    "@sapui5/sap.viz"
+  ].forEach((pkg) => {
+    const src = path.join(frameworkRoot, pkg, "1.124.0", "src");
+    if (fs.existsSync(src)) {
+      app.use("/resources", express.static(src));
+    }
+  });
 
   return cds.server({ ...options, app });
 };
